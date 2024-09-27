681a4abf47f75fc181637118e7846d3149c16b75
ZabdiAG <aguilera.glz.luis@gmail.com>
2024-09-27 11:29:42 -0600

    update shell.nix

--- config/shell.nix
+++ config/shell.nix
@@ -5,27 +5,43 @@ let
   pkgs = import nixpkgs { config = {}; overlays = []; };
 in pkgs.mkShellNoCC {
   packages = with pkgs; [
+    # std cli tools
     cowsay
     lolcat
     neovim
     ripgrep
+    jq
+    wget
+    tree
+    tree-sitter
     git
     tmux
     fzf
+
+    # Golang
     go
     gopls
 
-
+    # Rust
     cargo
     rustc
     rust-analyzer
 
+    # Ruby
+    ruby_3_3
 
     lua
     google-cloud-sdk
 
+    # All k8s related
+    kubectl
+    kubernetes-helm
+
+    # Containers
     lima
     podman
+
+    docker-client
 
     (pkgs.python3.withPackages (python-pkgs: [
       # select Python packages here
