a59d01402547e5613834ccc6d359a9bab475faab
ZabdiAG <aguilera.glz.luis@gmail.com>
2025-02-08 14:55:59 -0600

    update shell.nix

--- config/shell.nix
+++ config/shell.nix
@@ -1,6 +1,6 @@
 # shell.nix
 let
-  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-23.11";
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.11";
 
   pkgs = import nixpkgs { config = {}; overlays = []; };
 in pkgs.mkShellNoCC {
